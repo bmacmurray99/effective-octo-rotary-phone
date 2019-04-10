b1b5edb1089ce257d037ea769c8e2ab3e633fb8b
bmacmurray99 <b.macmur@gmail.com>
2019-04-09 23:21:23 -0400

    updated pinstest.py

--- raspi_files/pinstest.py
+++ raspi_files/pinstest.py
@@ -7,30 +7,14 @@ channels = [17,18]
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channels, GPIO.IN)
 #Activate Relays
-time.sleep(1)
-GPIO.setup(channels, GPIO.OUT)
-time.sleep(1)
-GPIO.setup(channels, GPIO.IN)
-time.sleep(1)
-GPIO.setup(channels, GPIO.OUT)
-time.sleep(1)
-GPIO.setup(channels, GPIO.IN)
-time.sleep(1)
-GPIO.setup(channels, GPIO.OUT)
-time.sleep(1)
-GPIO.setup(channels, GPIO.IN)
-time.sleep(1)
-GPIO.setup(channels, GPIO.OUT)
-time.sleep(1)
-GPIO.setup(channels, GPIO.IN)
-time.sleep(1)
-GPIO.setup(channels, GPIO.OUT)
-time.sleep(1)
-GPIO.setup(channels, GPIO.IN)
-time.sleep(1)
-GPIO.setup(channels, GPIO.OUT)
-time.sleep(1)
-GPIO.setup(channels, GPIO.IN)
+i = 30
+while i> 0 :
+  time.sleep(.5)
+  GPIO.setup(channels, GPIO.OUT)
+  time.sleep(.5)
+  GPIO.setup(channels, GPIO.IN)
+  i-=1
+
 
 
 
